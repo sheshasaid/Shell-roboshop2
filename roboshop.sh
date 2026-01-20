2e4a656a1dd5c67f960ec1a5f25b16e13a91b61d
Sheshasai Dyavanapally <Sheshasai>
2026-01-20 16:28:00 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -8,9 +8,9 @@ do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-078f63d14c419cf3c --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
     if [instance != "frontend" ]; then
-        aws ec2 describe-instances --instance-ids i-0fce07e0f753c5e4d --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        (aws ec2 describe-instances --instance-ids i-0fce07e0f753c5e4d --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
     else
-        aws ec2 describe-instances --instance-ids i-0fce07e0f753c5e4d --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        (aws ec2 describe-instances --instance-ids i-0fce07e0f753c5e4d --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
     fi
     echo "$instance: $IP"
 done
