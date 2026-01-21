d299f952ab1c3867d9ab460adca718cb86ef9133
Sheshasai Dyavanapally <Sheshasai>
2026-01-21 15:38:29 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -5,7 +5,7 @@ SG_ID="sg-063f4a8b92c8783d5"
 
 for instance in $@
 do
-    INSTANCEID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
     if [ $instance != "forntend" ]; then
         $IP=(aws ec2 describe-instances --instance-ids $INSTANCE_ID  --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
